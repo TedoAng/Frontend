c6a896c4667f1381da59d5278c1266bd3f462db5
Teodor Angelov <teodor.angelov@fastcomet.com>
2023-02-17 14:13:55 +0200

    format fix

--- Pie chart/app.js
+++ Pie chart/app.js
@@ -2,21 +2,13 @@ const unit = document.querySelector('.pie');
 
 console.log(unit.getAttribute('style'));
 
-
-
-
 let numb = 0;
 
-const refreshId = setInterval(()=>{
-    numb++
+const refreshId = setInterval(() => {
+    numb++;
     unit.style = `--p:${numb};--b:10px;--c:purple`;
     console.log(numb);
-    if(numb === 100) {
+    if (numb === 100) {
         clearInterval(refreshId);
     }
-},300)
-
-
-
-
-
+}, 300);
